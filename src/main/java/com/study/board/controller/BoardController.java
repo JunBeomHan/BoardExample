9e4d60ddf9d3a9ba44d50d6aca1a44da86cbf6a8
JunBumHan <2hanjunbum6@gmail.com>
2023-04-07 21:05:20 +0900

    Create Post Data

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,7 +1,8 @@
 package com.study.board.controller;
-
+import com.study.board.entity.Board;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
@@ -9,5 +10,11 @@ public class BoardController {
     @GetMapping("/board/write")
     public String boardStringForm() {
         return "boardWrite";
+    }
+
+    @PostMapping("/board/writePro")
+    public String boardWritePro(Board board) {
+        System.out.println(board.getTitle());
+        return "";
     }
 }
